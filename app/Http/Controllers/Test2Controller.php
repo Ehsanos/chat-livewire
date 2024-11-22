e76249f1310e7aad60653a51da6e17f43b53a423
Ehsano Gamor <ehsan.gamor90@gmail.com>
2024-11-22 22:06:54 +0300

    adding new model2

---
+++ app/Http/Controllers/Test2Controller.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Test2;
+use App\Http\Requests\StoreTest2Request;
+use App\Http\Requests\UpdateTest2Request;
+
+class Test2Controller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTest2Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Test2 $test2)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Test2 $test2)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTest2Request $request, Test2 $test2)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Test2 $test2)
+    {
+        //
+    }
+}
